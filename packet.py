3c547f5a6f9b208e7f5d3ebaec75e4a361f235c1
eginatakasan <13517030@std.stei.itb.ac.id>
2019-09-25 18:57:14 +0700

    baru nyadar, bytes itu  cuma string

--- packet.py
+++ packet.py
@@ -4,9 +4,9 @@ DATA_LENGTH = DATA_DIVIDE_LENGTH
 def MakePacket(int_id, file):
     packet = []
 
-    binary_file = convertBinary(file)
+    binary_file = convert_file_to_binary(file)
     remainder = len(binary_file) % DATA_DIVIDE_LENGTH
-    data = data_divider(binary_file)
+    data = data_divider(binary_file, DATA_DIVIDE_LENGTH)
 
     types = get_packet_types(len(data))
     id = get_packet_id(len(data), int_id)
@@ -14,35 +14,25 @@ def MakePacket(int_id, file):
     sequence = get_packet_sequence(len(data))
     length = get_packet_length(len(data), remainder)
 
-    print("types:")
-    print (types)
-    print("id:")
-    print(id)
-    print("sequence:")
-    print (sequence)
-    print("length:")
-    print (length)
-    print("data index-4:")
-    print(data[4])
+    get_packet_checksum(types, id, sequence, length, data)
 
-def convertBinary(fileName):
+def convert_file_to_binary(fileName):
     f = open(fileName, 'rb')
     file_content = f.read()
     f.close()
     return file_content
 
-def data_divider(data):
-    data = [data[i:i + DATA_DIVIDE_LENGTH] for i in range(0, len(data), DATA_DIVIDE_LENGTH)]
-    return data
-
 def convert_int_to_binary(x, width):
     input = x
     return bytes((map((int), [x for x in '{:0{size}b}'.format(input,size=width)])))
 
-def print_iterator(it):
-    output = []
-    for x in it:
-        print(x)
+def convert_binary_to_int(bin):
+    return int.from_bytes(bin)
+
+def data_divider(data, width):
+    data = [data[i:i + width] for i in range(0, len(data), width)]
+    return data
+
 
 def get_packet_types(len):
     types = []
@@ -76,6 +66,24 @@ def get_packet_length(len, remainder):
     return length
 
 def get_packet_checksum(type, id, sequence, length, data):
-    print()
+    checksum = 0
+    row_zero_adder = bin([0,0,0,0,0,0,0,0])
+
+    for x in range(0,len(data)):
+        first_row = (type[x] + data[x])
+        row_sequence = data_divider(sequence[x],1)
+        row_length = data_divider(length[x],1)
+        row_data = (data_divider(data[x],1))
+
+        checksum ^= first_row
+        for row in row_sequence:
+            checksum ^= row
+        for row in row_length:
+            checksum ^= row
+        for row in row_data:
+            checksum ^= row
+
+    return checksum
+
 
 MakePacket(4,"a.jpg")
