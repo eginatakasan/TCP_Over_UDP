a592b59715df70a4f472d1c31cbf9a47c11609c2
eginatakasan <13517030@std.stei.itb.ac.id>
2019-09-25 14:50:29 +0700

    made id, type, length, sequence packet function

--- packet.py
+++ packet.py
@@ -1,14 +1,29 @@
-DATA_DIVIDE_LENGTH = 1024 * 32
+DATA_DIVIDE_LENGTH = 32768
 DATA_LENGTH = DATA_DIVIDE_LENGTH
 
-def MakePacket(id, file):
+def MakePacket(int_id, file):
     packet = []
 
-    data = convertBinary(file)
-    types = get_packet_types(len(data))
-    
-    sequence = []
+    binary_file = convertBinary(file)
+    remainder = len(binary_file) % DATA_DIVIDE_LENGTH
+    data = data_divider(binary_file)
 
+    types = get_packet_types(len(data))
+    id = get_packet_id(len(data), int_id)
+    
+    sequence = get_packet_sequence(len(data))
+    length = get_packet_length(len(data), remainder)
+
+    print("types:")
+    print (types)
+    print("id:")
+    print(id)
+    print("sequence:")
+    print (sequence)
+    print("length:")
+    print (length)
+    print("data index-4:")
+    print(data[4])
 
 def convertBinary(fileName):
     f = open(fileName, 'rb')
@@ -18,20 +33,49 @@ def convertBinary(fileName):
 
 def data_divider(data):
     data = [data[i:i + DATA_DIVIDE_LENGTH] for i in range(0, len(data), DATA_DIVIDE_LENGTH)]
-    data.append("END")
     return data
+
+def convert_int_to_binary(x, width):
+    input = x
+    return bytes((map((int), [x for x in '{:0{size}b}'.format(input,size=width)])))
+
+def print_iterator(it):
+    output = []
+    for x in it:
+        print(x)
 
 def get_packet_types(len):
     types = []
     for x in range(0, len):
         if (x == len-1): 
-            types.append(bin(0x2)) #type FIN
+            types.append(convert_int_to_binary(0x2, 4)) #type FIN
         else:
-            types.append(bin(0x0)) #type DATA
+            types.append(convert_int_to_binary(0x0, 4)) #type DATA
     return types
+
+def get_packet_id(len, int_id):
+    id = convert_int_to_binary(int_id,4)
+    list_id = []
+    for x in range(0, len):
+        list_id.append(id)
+    return list_id
 
 def get_packet_sequence(len):
     seq = []
     for x in range(0, len):
-        seq.append(x)
+        seq.append(convert_int_to_binary(x, 16))
     return seq
+
+def get_packet_length(len, remainder):
+    length = []
+    for x in range(0, len):
+        if (x == len-1):
+            length.append(convert_int_to_binary(remainder, 16))
+        else:
+            length.append(convert_int_to_binary(DATA_DIVIDE_LENGTH, 16))
+    return length
+
+def get_packet_checksum(type, id, sequence, length, data):
+    print()
+
+MakePacket(4,"a.jpg")
