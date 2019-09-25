9079caa514ad8655be33b150f8cc0d1f1eb37a88
eginatakasan <13517030@std.stei.itb.ac.id>
2019-09-25 08:18:01 +0700

    apa yang kukerjain semalam lah

--- packet.py
+++ packet.py
@@ -1,18 +1,37 @@
 DATA_DIVIDE_LENGTH = 1024 * 32
 DATA_LENGTH = DATA_DIVIDE_LENGTH
 
+def MakePacket(id, file):
+    packet = []
+
+    data = convertBinary(file)
+    types = get_packet_types(len(data))
+    
+    sequence = []
+
+
 def convertBinary(fileName):
-    f = open(fileName, 'rb');
+    f = open(fileName, 'rb')
     file_content = f.read()
-    print(len(data_divider(file_content)))
     f.close()
+    return file_content
 
 def data_divider(data):
     data = [data[i:i + DATA_DIVIDE_LENGTH] for i in range(0, len(data), DATA_DIVIDE_LENGTH)]
     data.append("END")
     return data
 
-def packet_type():
-    if 
+def get_packet_types(len):
+    types = []
+    for x in range(0, len):
+        if (x == len-1): 
+            types.append(bin(0x2)) #type FIN
+        else:
+            types.append(bin(0x0)) #type DATA
+    return types
 
-print(convertBinary("a.jpg"))
+def get_packet_sequence(len):
+    seq = []
+    for x in range(0, len):
+        seq.append(x)
+    return seq
