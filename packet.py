8c554895af630e5b4868de035129c20ac6becccf
eginatakasan <13517030@std.stei.itb.ac.id>
2019-09-25 22:28:05 +0700

    divide packet udah bisa yey, tinggal hitung checksum

--- packet.py
+++ packet.py
@@ -27,63 +27,74 @@ def convert_int_to_binary(x, width):
     return bytes((map((int), [x for x in '{:0{size}b}'.format(input,size=width)])))
 
 def convert_binary_to_int(bin):
-    return int.from_bytes(bin)
+    return int.from_bytes(bin, byteorder='big')
 
 def data_divider(data, width):
     data = [data[i:i + width] for i in range(0, len(data), width)]
     return data
 
+# def fix_size(x, width):
+    
 
 def get_packet_types(len):
     types = []
     for x in range(0, len):
         if (x == len-1): 
-            types.append(convert_int_to_binary(0x2, 4)) #type FIN
+            types.append(0x2) #type FIN
         else:
-            types.append(convert_int_to_binary(0x0, 4)) #type DATA
+            types.append(0x0) #type DATA
     return types
 
 def get_packet_id(len, int_id):
-    id = convert_int_to_binary(int_id,4)
     list_id = []
     for x in range(0, len):
-        list_id.append(id)
+        list_id.append(int_id)
     return list_id
 
 def get_packet_sequence(len):
     seq = []
     for x in range(0, len):
-        seq.append(convert_int_to_binary(x, 16))
+        seq.append(x)
     return seq
 
 def get_packet_length(len, remainder):
     length = []
     for x in range(0, len):
         if (x == len-1):
-            length.append(convert_int_to_binary(remainder, 16))
+            length.append(remainder)
         else:
-            length.append(convert_int_to_binary(DATA_DIVIDE_LENGTH, 16))
+            length.append(DATA_DIVIDE_LENGTH)
     return length
 
 def get_packet_checksum(type, id, sequence, length, data):
     checksum = 0
-    row_zero_adder = bin([0,0,0,0,0,0,0,0])
+    list_bytes = []
 
     for x in range(0,len(data)):
-        first_row = (type[x] + data[x])
-        row_sequence = data_divider(sequence[x],1)
-        row_length = data_divider(length[x],1)
-        row_data = (data_divider(data[x],1))
+        first_row = (type[x] << 4) + id[x]
+        row_sequence = sequence
+        row_length = length
+        row_data = data_divider(data[x],1)
 
-        checksum ^= first_row
-        for row in row_sequence:
-            checksum ^= row
-        for row in row_length:
-            checksum ^= row
+        temp = (convert_int_to_binary((type[x] << 4) + id[x], 8))
+        temp +=(convert_int_to_binary(sequence[x], 16))
+        temp +=(convert_int_to_binary(length[x], 16))
         for row in row_data:
-            checksum ^= row
+            temp += row
+        list_bytes.append(temp)
 
-    return checksum
+        data_divider(list_bytes, 8)
+
+    #     checksum ^= first_row
+    #     for row in row_sequence:
+    #         checksum ^= row
+    #     for row in row_length:
+    #         checksum ^= row
+    #     for row in row_data:
+    #         checksum ^= convert_binary_to_int(row)
+    # print(checksum)
+
+    # return checksum
 
 
 MakePacket(4,"a.jpg")
