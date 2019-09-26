8e0b4c1353456a1b701e7a3195a14859c07ab43e
xinzuan <veldazelda15@gmail.com>
2019-09-26 14:12:10 +0700

    ubah client

--- Client.py
+++ Client.py
@@ -1,14 +1,41 @@
 import socket
 import threading
+from packet import *
+import os
 
-HOST ='127.0.0.1'
-PORT = 6134 
+# HOST ='127.0.0.1'
+# PORT = 6134 
+TIMED_OUT = 2
+int_id =2
+package =[]
+progressBar = 0
+BUFFER = 4096
+def sender (host,port,file_name):
 
-def send():
+    port_int = int(port)
+    file_size = os.path.getsize(file_name) 
+    p = Packet()
+    package = MakePackets(2,file_name)
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.sendto("halo".encode(),0,(HOST,PORT))
+        for a in package:
+            try:
+                print('Sending package ...')
+                s.sendto(p.combine_rows(),0,(host,port_int))
+                # if (s.sendto(p,0,(host,port_int))):
+
+                s.settimeout(TIMED_OUT)
+
+                print('Waiting..')
+                # data,addr = s.recvfrom(BUFFER)
+                print('Sending next package... ')
+            except s.timeout :
+                print("Sorry... failed to send file")
+        
+
 
 if __name__ == '__main__':
+    host = input("Address : ")
+    port = input("Port : ")
     n_file = int(input("Jumlah file yang ingin dikirim: "))
     print('Tekan 1 untuk inputan file berupa nama file')
     print('Tekan 2 untuk inputan file berupa path file')
@@ -16,20 +43,20 @@ if __name__ == '__main__':
     files=[]
     threads=[]
 
-    if (n<=5):
+    if (n_file<=5):
         #threading utk banyak files
-        for i in range n_file:
+        for i in range(n_file):
             jenis = int(input("Nomor"))
             if (jenis ==1):              
-                nama_file = input("Nama file: ")
+                file_name = input("Nama file: ")
                 files.append(file_name)
             if (jenis ==2):
                 path_file = input('Path file : ')
                 file_name = os.path.basename(path_file)
                 files.append(file_name)
-        
-        for f in range files:
-            thread = threading.Thread(target=sender, args=())
+
+        for f in files:
+            thread = threading.Thread(target=sender, args=(host,port,f))
             thread.start()
             threads.append(thread)
 
